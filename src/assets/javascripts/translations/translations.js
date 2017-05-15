126598b917eb32400a73837d51455138c39dfa18
tsi <tsi.shlidor@gmail.com>
2017-05-15 13:15:13 +0300

    Translations

--- src/assets/javascripts/translations/translations.js
+++ src/assets/javascripts/translations/translations.js
@@ -1,4 +1,86 @@
+
+
 export const translations = {
   en: {
+    // Home page
+    'PageSpeedAppName': 'Page Speed Analyzer',
+    'PageTitleHome': 'Page Speed Image Analysis Tool',
+    'LearnMoreText': 'Learn More',
+    'LearnMoreURL': 'http://cloudinary.com/features/image_manipulation',
+    'PoweredByText': 'Powered By',
+    'CloudinaryLogoURL': 'http://cloudinary.com',
+    'ToolDescription': 'Every image is unique and every one of your website visitors is different. In a perfect world, we would like to adapt each image individually to be "just right" for every user, by being perfectly cropped, with correct encoding settings and optimal quality with the best image format.',
+    'EditBoxDefaultText': 'Enter URL',
+    'ButtonText': 'START',
+
+    // Page Footer
+    'FooterTitle': 'Optimize Every Image',
+    'FooterText': 'The following sample image presents the single line of code needed to deliver an optimized image to your users, including on-the-fly transformations:',
+    'FooterOriginalTitle': 'Original',
+    'FooterCompressedTitle': 'Dynamic Format',
+    'FooterOriginalImageFormat': 'JPG',
+    'FooterCompressedImageFormat': 'WEBP',
+    'FooterOriginalImageDimensions': '800x450',
+    'FooterCompressedImageDimensions': '800x450',
+    'FooterOriginalImageWeight': '73 KB',
+    'FooterCompressedImageWeight': '53 KB (saved 28%)',
+    'FooterSampleURL': 'https://res.cloudinary.com/pagespeed/w_800,f_auto,q_auto/paint.jpg',
+    'TryItNowButtonText': 'Try Now',
+    'TryItNowButtonURL': 'https://cloudinary.com/signup',
+    'AboutButtonText': 'About',
+    'AboutButtonURL': 'http://cloudinary.com/features/image_manipulation',
+    'PrivacyButtonText': 'Privacy',
+    'PrivacyButtonURL': 'http://cloudinary.com/privacy',
+    'TermsButtonText': 'Terms',
+    'TermsButtonURL': 'http://www.responsivebreakpoints.com/tos',
+    'LinkedinURL': 'https://www.linkedin.com/company/cloudinary',
+    'FacebookURL': 'https://www.facebook.com/Cloudinary',
+    'GooglePlusURL': 'https://plus.google.com/+Cloudinary',
+    'TwitterURL': 'https://twitter.com/cloudinary',
+
+    /* Results page
+    **********************/
+
+    // Page header
+    'PageTitleResults': 'Image Analysis Results',
+    'ShareResults': 'Share Results',
+    'PageImageScoreTitle': 'Page Image Score',
+    'ImageWeightComparisonTitle': 'Image Weight Comparison',
+    'OriginalImages': 'Original Images',
+    'PotentialCompression': 'Potential Smart Compression Weight',
+    'AverageGradeA': 'Average Grade A',
+    'AverageGradeB': 'Average Grade B',
+    'AverageGradeC': 'Average Grade C',
+    'AverageGradeD': 'Average Grade D',
+    'AverageGradeE': 'Average Grade E',
+    'AverageGradeF': 'Average Grade F',
+    'TotalImagesNumber': 'Total Image Count',
+    'TotalImagesWeight': 'Total Image Weight',
+    'PotentialCompressionPercentage': 'Potential Smart Compression Percentage',
+    'TotalImagesNumber_TooMany_A': 'Only first', // Only first X images were analyzed
+    'TotalImagesNumber_TooMany_B': 'images were analyzed',
+    'Tested_BrowserType': 'Tested on browser',
+    'Tested_Location': 'From location',
+    'Tested_Viewport': 'Viewport size',
+    'Tested_DeviceDPI': 'Device DPI',
+    'IntegratedWith': 'Integrated with',
+
+    // Page body (collapse and expanded)
+    'CollapsedPotentialCompressionTitle': 'Potential Smart Compression',
+    'ExpandButton': 'See More',
+    'CollapseButton': 'Close',
+    'BestImageText': 'Smallest Image',
+    'ExpandedTabOriginal': 'Original',
+    'ExpandedTabSameFormat': 'Transformed Image',
+    'ExpandedTabOtherFormats': 'Dynamic Format Selection',
+    'ImageProperty_Aggregated': 'Aggregated',
+    'ImageProperty_Format': 'Format',
+    'ImageProperty_Fit': 'FIT',
+    'ImageProperty_Compression': 'Compression',
+    'ImageProperty_ColorSpace': 'Color Space',
+    'ImageProperty_Color  Depth': 'Color Depth',
+    'ImageProperty_Metadata': 'Metadata',
+    'ImageProperty_Progressive': 'Progressive',
+    'GradesToAverageConnection': 'Average'
   }
 }
