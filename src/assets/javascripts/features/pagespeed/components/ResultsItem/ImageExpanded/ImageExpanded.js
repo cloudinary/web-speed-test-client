e60b47405929d21ddf1f83e01f33c7c018995fa5
tsi <tsi.shlidor@gmail.com>
2017-05-16 13:11:38 +0300

    Tabs

--- src/assets/javascripts/features/pagespeed/components/ResultsItem/ImageExpanded/ImageExpanded.js
+++ src/assets/javascripts/features/pagespeed/components/ResultsItem/ImageExpanded/ImageExpanded.js
@@ -1,7 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Image, Transformation } from 'cloudinary-react';
-import numbro from 'numbro';
-import classnames from 'classnames';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import './ImageExpanded.scss';
 
@@ -18,16 +16,32 @@ export default class ImageExpanded extends Component {
           <div className="title">
             {this.context.t('ExpandedTabOriginal')}
           </div>
+          <div className="tab">
+            {result.format}
+          </div>
         </div>
         <div className="image-details transformed">
           <div className="title">
             {this.context.t('ExpandedTabSameFormat')}
+          </div>
+          <div className="tab">
+            {result.transformedImage.analyze.data.format}
           </div>
         </div>
         <div className="image-details dynamic">
           <div className="title">
             {this.context.t('ExpandedTabOtherFormats')}
           </div>
+          <Tabs>
+            <TabList>
+              {result.dynamicFormats.map((format, key) => (
+                <Tab key={key}>{format.analyze.data.format}</Tab>
+              ))}
+            </TabList>
+            {result.dynamicFormats.map((format, key) => (
+              <TabPanel key={key}>{format.analyze.data.format}</TabPanel>
+            ))}
+          </Tabs>
         </div>
       </div>
     );
