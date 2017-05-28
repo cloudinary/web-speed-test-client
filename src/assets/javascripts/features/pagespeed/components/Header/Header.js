af7a28888e203a079d7cc90bf28cb01b70f2a40e
tsi <tsi.shlidor@gmail.com>
2017-05-28 16:18:18 +0300

    Share widgets

--- src/assets/javascripts/features/pagespeed/components/Header/Header.js
+++ src/assets/javascripts/features/pagespeed/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Image, Transformation } from 'cloudinary-react';
+import Share from '../Share/Share';
 
 import './Header.scss';
 
@@ -11,6 +12,7 @@ export default class Header extends Component {
           <Image className="logo" publicId="icon-logo.svg.svg" width="31" type="asset"></Image>
           <h1>{this.context.t("PageTitleA")}</h1>
           <a className="learn" href={this.context.t('LearnMoreURL')} target="_blank">{this.context.t("LearnMoreText")}</a>
+          <Share shareUrl={window.location.href.split('?')[0]} title={this.context.t("PageTitleA") + ' - ' + this.context.t('PageTitleB')} />
           <a className="powered-by" href={this.context.t('CloudinaryLogoURL')} target="_blank">
             <div className="label">{this.context.t('PoweredByText')}</div>
             <Image className="logo" publicId="cloudinary_logo_text.svg.svg" width="120" type="asset"></Image>
