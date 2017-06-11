5deeb8fbe6d1bba4ea2eab0bba542659f44f6d64
tsi <tsi.shlidor@gmail.com>
2017-06-11 17:45:03 +0300

    Analytics

--- src/assets/javascripts/app/Root.js
+++ src/assets/javascripts/app/Root.js
@@ -3,18 +3,21 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router';
-
 import routes from './routes';
 
-// If you use React Router, make this component
-// render <Router> with your routes. Currently,
-// only synchronous routes are hot reloaded, and
-// you will see a warning from <Router> on every reload.
-// You can ignore this warning. For details, see:
-// https://github.com/reactjs/react-router/issues/2182
-
+// Analytics
+import ReactGA from 'react-ga';
+if (process.env.GA) {
+  ReactGA.initialize(process.env.GA);
+}
 
 const Root = ({ store, history }: any) => {
+
+  if (process.env.GA) {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
   let ComponentEl = (
     <Provider store={store}>
       <Router history={history} routes={routes} />
