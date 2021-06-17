e2eb78c958d3b33522e7acbea1ea8bf849b2f1dc
Tsachi Shlidor <tsi.shlidor@gmail.com>
2021-06-17 15:09:19 +0300

    re-component and fix (most) warnings

--- src/App.js
+++ src/App.js
@@ -1,105 +1,20 @@
-import React, {Fragment, useEffect} from 'react';
-import { BrowserRouter as Router, Route, Switch, useLocation, useHistory, useParams, useRouteMatch } from 'react-router-dom';
-// import GoogleTagManager from './GoogleTagManager/GoogleTagManager'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { CloudinaryContext } from 'cloudinary-react';
+// import GoogleTagManager from './GoogleTagManager/GoogleTagManager'
 // import ReactGA from 'react-ga';
-import { useStore } from 'store/context';
 
 import 'services/i18n';
 
 import Header from 'components/Header/Header';
-import InputUrl from 'components/InputUrl/InputUrl';
-import Loader from 'components/Loader/Loader';
-import Error from 'components/Error/Error';
-import ResultSumm from 'components/ResultSumm/ResultSumm';
-import ResultsList from 'components/ResultsList/ResultsList';
 import PreFooter from 'components/PreFooter/PreFooter';
 import Footer from 'components/Footer/Footer';
 import StaticPage from 'views/StaticPage/StaticPage';
+import WebSpeedPage from 'views/WebSpeedPage/WebSpeedPage';
 
 import 'styles/styles.scss';
-import {fetchTestDataIfNeeded, runNewTest} from './store/actions';
-
-function WebSpeedPage(props) {
-  const {
-    dispatch,
-    state: { webspeedtest },
-  } = useStore();
-
-  const { search, pathname } = useLocation();
-  const history = useHistory();
-  const { testId: paramTestId } = useParams();
-  const storeTestId = webspeedtest.testId;
-
-  useEffect(() => {
-    const locationParams = new URLSearchParams(search);
-    const locationTestId = locationParams.get('testId')
-    const testId = paramTestId ? paramTestId : locationTestId;
-    if (locationTestId && !paramTestId) {
-      history.push({
-        pathname: pathname + "results/" + locationTestId
-      });
-    }
-    dispatch({
-      type: 'setTestId',
-      testId: testId,
-    });
-    fetchTestDataIfNeeded(testId, dispatch, webspeedtest);
-  }, [search, pathname, paramTestId]);
-
-  useEffect(() => {
-    if (storeTestId && (pathname.indexOf('results') == -1)) {
-      history.push({
-        pathname: pathname + "results/" + storeTestId
-      })
-    }
-  }, [storeTestId, pathname])
-
-  return <Fragment>
-    {!webspeedtest.testId && (
-        <InputUrl
-            onSubmit={async (url) => {
-              runNewTest(url, dispatch, webspeedtest);
-            }}
-        />
-    )}
-    {webspeedtest.testId && webspeedtest.isFetching !== false && (
-        <Loader url={webspeedtest.testUrl} />
-    )}
-    {webspeedtest.testId &&
-    webspeedtest.isFetching === false &&
-    !webspeedtest.error && (
-        <div className="page-wrap">
-          <ResultSumm
-              testId={webspeedtest.testId}
-              result={webspeedtest.testResult.resultSumm || {}}
-          />
-          {webspeedtest.testResult.resultSumm.totalImagesCount >
-          0 && (
-              <ResultsList
-                  testId={webspeedtest.testId}
-                  results={
-                    webspeedtest.testResult.imagesTestResults || []
-                  }
-              />
-          )}
-        </div>
-    )}
-    {webspeedtest.error && <Error error={webspeedtest.error} />}
-
-  </Fragment>
-}
 
 function App(props) {
-  // ToDo: add fetchTestData logic.
-  // Old implementation in src/components/WebspeedtestView.js
-
-  const {
-    dispatch,
-    state: { webspeedtest },
-  } = useStore();
-
-
   return (
     <CloudinaryContext
       cloudName={process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}
@@ -112,12 +27,12 @@ function App(props) {
         <Header />
 
         <Switch>
-          <Route exact path={["/results/:testId", "/"]}>
-            <WebSpeedPage/>
+          <Route exact path={['/results/:testId', '/']}>
+            <WebSpeedPage />
           </Route>
           <Route
-              path="/:page"
-              render={(props) => <StaticPage page={props.match.params.page} />}
+            path="/:page"
+            render={(props) => <StaticPage page={props.match.params.page} />}
           />
         </Switch>
 
@@ -128,4 +43,4 @@ function App(props) {
   );
 }
 
-export default App;
+export default React.memo(App);
---
+++ src/views/WebSpeedPage/WebSpeedPage.js
@@ -0,0 +1,82 @@
+import React, { Fragment, useEffect } from 'react';
+import { useLocation, useHistory, useParams } from 'react-router-dom';
+// import ReactGA from 'react-ga';
+import { useStore } from 'store/context';
+
+import InputUrl from 'components/InputUrl/InputUrl';
+import Loader from 'components/Loader/Loader';
+import Error from 'components/Error/Error';
+import ResultSumm from 'components/ResultSumm/ResultSumm';
+import ResultsList from 'components/ResultsList/ResultsList';
+
+import { fetchTestDataIfNeeded, runNewTest } from 'store/actions';
+
+function WebSpeedPage(props) {
+  const {
+    dispatch,
+    state: { webspeedtest },
+  } = useStore();
+
+  const { search, pathname } = useLocation();
+  const history = useHistory();
+  const { testId: paramTestId } = useParams();
+  const storeTestId = webspeedtest.testId;
+
+  useEffect(() => {
+    const locationParams = new URLSearchParams(search);
+    const locationTestId = locationParams.get('testId');
+    const testId = paramTestId ? paramTestId : locationTestId;
+    if (locationTestId && !paramTestId) {
+      history.push({
+        pathname: pathname + 'results/' + locationTestId,
+      });
+    }
+    dispatch({
+      type: 'setTestId',
+      testId: testId,
+    });
+    fetchTestDataIfNeeded(testId, dispatch, webspeedtest);
+  }, [search, pathname, paramTestId, dispatch, history]);
+
+  useEffect(() => {
+    if (storeTestId && pathname.indexOf('results') === -1) {
+      history.push({
+        pathname: pathname + 'results/' + storeTestId,
+      });
+    }
+  }, [storeTestId, pathname, history]);
+
+  return (
+    <Fragment>
+      {!webspeedtest.testId && (
+        <InputUrl
+          onSubmit={async (url) => {
+            runNewTest(url, dispatch, webspeedtest);
+          }}
+        />
+      )}
+      {webspeedtest.testId && webspeedtest.isFetching !== false && (
+        <Loader url={webspeedtest.testUrl} />
+      )}
+      {webspeedtest.testId &&
+        webspeedtest.isFetching === false &&
+        !webspeedtest.error && (
+          <div className="page-wrap">
+            <ResultSumm
+              testId={webspeedtest.testId}
+              result={webspeedtest.testResult.resultSumm || {}}
+            />
+            {webspeedtest.testResult.resultSumm.totalImagesCount > 0 && (
+              <ResultsList
+                testId={webspeedtest.testId}
+                results={webspeedtest.testResult.imagesTestResults || []}
+              />
+            )}
+          </div>
+        )}
+      {webspeedtest.error && <Error error={webspeedtest.error} />}
+    </Fragment>
+  );
+}
+
+export default React.memo(WebSpeedPage);
