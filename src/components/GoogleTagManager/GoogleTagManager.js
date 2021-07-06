e35dbf53a49c2f1743bbfa63f178328d860d2194
Tsachi Shlidor <tsi.shlidor@gmail.com>
2021-07-06 10:50:21 +0300

    linter

--- src/components/GoogleTagManager/GoogleTagManager.js
+++ src/components/GoogleTagManager/GoogleTagManager.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import gtmParts from 'react-google-tag-manager';
 
 export default class GoogleTagManager extends Component {
