ba0b4b3597c43e90ed4f7a6052f362ecaec132fb
Łukasz Czerpak <lukasz.czerpak@cloudinary.com>
2023-12-19 15:22:46 +0100

    chore: changed output dirs for assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths"
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import {ViteMinifyPlugin} from 'vite-plugin-minify'
 
 export default defineConfig({
     base: '/',
@@ -14,5 +14,20 @@ export default defineConfig({
     server: {
         open: true,
         port: 3000,
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    if (/css/i.test(extType)) {
+                        extType = 'css';
+                    }
+                    return `static/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'static/js/[name]-[hash].js',
+                entryFileNames: 'static/js/[name]-[hash].js',
+            },
+        },
     },
 });
