ff1bce6d2f8b26f2e48ceb1f78717d57c3126217
Łukasz Czerpak <89903002+lukaszczerpak-cloudinary@users.noreply.github.com>
2024-05-22 16:29:27 +0200

    security patches and dependencies updates
    - fix: security patches - SNYK-JS-BRACES-6838727 and SNYK-JS-MICROMATCH-6838728 (#77)
    - chore: update React to v18
    - chore: update other deps to latest versions

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { StoreProvider } from './store/context';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <StoreProvider>
       <Router>
         <App />
       </Router>
     </StoreProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
